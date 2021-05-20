4608e54dc1576a30da5ffb4d20e7596ca0e9800b
Virginijus03 <klimauskas@yahoo.com>
2021-05-20 20:11:09 +0300

    masinos nebaigtas

---
+++ js/ratines/Transportas/Transportas.js
@@ -0,0 +1,24 @@
+
+class Trasportas {
+    constructor(name, handWheelPosition, seatCount, wheelCount){
+        this.name = name;
+        this.handWheelPosition =  handWheelPosition;
+        this.seatCount = seatCount;
+        this.wheelCount = wheelCount;
+    }
+/*
+ (p: vairo pozicija, sedyniu skaicius,ar ijungtas varyklis, ratu skaicius;
+m: ijungti varykli, isjungti varykli)*/
+
+engineRun(){
+    this.egineRun = false;
+    console.log(engineRun);
+}
+
+engineRun(){
+    this.engineRun = true;
+}
+
+}
+
+module.exports = Transportas;
---
+++ js/ratines/index.js
@@ -0,0 +1,25 @@
+
+/*
+const Dog = require('./js/Dog');
+const Cat = require('./js/Cat');
+const Rabbit = require('./js/Rabbit');
+
+const rexas = new Dog('Rex', 'brown');
+rexas.voice();
+
+const rainis = new Cat('Rainis', 'golden');
+rainis.voice();
+
+const benas = new Rabbit('Benas', 'black');
+benas.voice();
+*/
+
+
+
+
+const BMW = require('./Lengvosios/BMW.JS');
+const BMW = new BMW('BMW');
+
+const Transportas = require('./Transportas/Transportas');
+const Transportas = new Trasportas('');
+
