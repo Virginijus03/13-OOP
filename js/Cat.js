8d8e2de9b0f8cc6b7cc2858b3518cbd9d0109c4b
Virginijus03 <klimauskas@yahoo.com>
2021-05-20 18:51:38 +0300

    paveldejimai

--- js/Cat.js
+++ js/Cat.js
@@ -1,20 +1,11 @@
-class Cat {
+  
+const Pet = require('./Pet');
+
+class Cat extends Pet {
     constructor(name, fur) {
-       this.name = name;
-       this.furColor = fur;
-       this.legsCount = 4;
-       this.hasTale = true;
-       this.voiceCount = 0;
-       this.sound = 'miau miau!';
+        super(name, fur);
+        this.sound = 'miau miau';
     }
-
-    voice() {
-        //this.voiceCount++
-        console.log(`${this.name}: $!(${++this.voiceCount})`);
-    
-
-    }
-
 }
 
 module.exports = Cat;
